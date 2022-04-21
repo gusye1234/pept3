d5662369ba079daf85c9f71dd36e691c973d3ff8
Jianbai Ye <jianbaiye@outlook.com>
2022-04-21 22:37:29 +0800

    add md5 check
    Signed-off-by: Jianbai Ye <jianbaiye@outlook.com>

--- tests/test_deps.py
+++ tests/test_deps.py
@@ -1,4 +1,5 @@
 import pytest
+from stu import models
 
 
 def test_deps():
@@ -6,3 +7,17 @@ def test_deps():
     import pandas
     import numpy
     import torch
+
+
+def test_checkpoints():
+    import hashlib
+    md5_book = {
+        'prosit': "ec4bc8a7761c38f8732f5f53c3ec40ff",
+        'pdeep': "ae4deb4efbc963c57bd1420ba5df109e"
+    }
+    for name, md5_v in md5_book.items():
+        weights = models.Model_Weights_Factories(name)
+        weights_bin = open(weights, 'rb')
+        md5_test = hashlib.md5()
+        md5_test.update(weights_bin.read())
+        assert md5_test.hexdigest() == md5_v

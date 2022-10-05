5e5db0e22502656e90cfcd6dd6129cbe53c9d0e2
Jianbai Ye <jianbaiye@outlook.com>
2022-10-05 19:17:36 +0800

    ci: fix pip deps
    Signed-off-by: Jianbai Ye <jianbaiye@outlook.com>

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 import setuptools
-from importlib_metadata import entry_points
 
-import tfpms
+vars2find = ['__author__', '__version__', '__url__']
+varstfp = {}
+with open("./tfpms/__init__.py") as f:
+    for line in f.readlines():
+        for v in vars2find:
+            if line.startswith(v):
+                line = line.replace(" ", '').replace(
+                    "\"", '').replace("\'", '').strip()
+                varstfp[v] = line.split('=')[1]
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
-with open('requirements.txt', 'r') as fh:
-    deps = [l.strip() for l in fh]
 
 setuptools.setup(
     name='tfpms',
-    url=tfpms.__url__,
-    version=tfpms.__version__,
-    author=tfpms.__author__,
+    url=varstfp['__url__'],
+    version=varstfp['__version__'],
+    author=varstfp['__author__'],
     description='Data-specific model fine-tuning for MS/MS spectrum prediction',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -23,7 +28,11 @@ setuptools.setup(
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
-    install_requires=deps,
+    install_requires=["h5py",
+                      "numpy",
+                      "pandas",
+                      "torch"
+                      ],
     entry_points={
         'console_scripts': [
             'tfpms = tfpms:main',
